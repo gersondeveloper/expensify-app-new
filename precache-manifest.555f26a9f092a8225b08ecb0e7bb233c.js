e5e18e550375496db9e989d0705dd0f1e83b839e
Gerson C Filho <gersoncfilho@mac.com>
2020-01-01 12:28:31 -0300

    Updates

---
+++ precache-manifest.555f26a9f092a8225b08ecb0e7bb233c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a39ad7c7c1adf1228a15c08bf018127",
+    "url": "/expensify-app-new/index.html"
+  },
+  {
+    "revision": "c90ee2201ccd40d6c3c7",
+    "url": "/expensify-app-new/static/js/2.84ca0a52.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/expensify-app-new/static/js/2.84ca0a52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d5dbbfcb4753b06c84c",
+    "url": "/expensify-app-new/static/js/main.1baf32c5.chunk.js"
+  },
+  {
+    "revision": "1a22f79d328819552b30",
+    "url": "/expensify-app-new/static/js/runtime-main.4276c70f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/expensify-app-new/precache-manifest.555f26a9f092a8225b08ecb0e7bb233c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/expensify-app-new/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
